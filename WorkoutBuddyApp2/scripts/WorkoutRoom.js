c8b4a0a268dc6d3dbb457b1eb2b71013a9179b19
Tai Won Chung <tchung1118@gmail.com>
2014-08-20 20:29:48 +0900

    Room view

---
+++ WorkoutBuddyApp2/scripts/WorkoutRoom.js
@@ -0,0 +1,23 @@
+var app = app || {};
+
+app.WorkoutRoomViewModel = (function () {
+    'use strict'
+
+    var workoutRoomViewModel = (function () {
+        
+        var init = function () {
+        };
+        
+        var show = function () {
+        };
+        
+        return {
+            init: init,
+            show: show
+        };
+        
+    }());
+    
+    return workoutRoomViewModel;
+    
+}());
